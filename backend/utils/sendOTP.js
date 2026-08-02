7ab4f9cb844db823e4436eea25a02ee690e6d79a
Ashraf Shaikh <ashrafshaikh5683@gmail.com>
2026-08-03 01:03:42 +0530

    nodemailer setup done for sendOTP

---
+++ backend/utils/sendOTP.js
@@ -0,0 +1,19 @@
+import transporter from "../config/nodemailer.js";
+const sendOTP=async(email, otp)=>{
+    try{
+        await transporter.sendMail({
+            from: process.env.EMAIL_USER,
+            to:email,
+            subject:"Your OTP for Login GymOpsFlow",
+            html:
+            `<h2>GymOpsFlow Login Verification</h2>
+            <p>Your OTP is : ${otp}</p>
+            <p>This OTP is valid for 5 minutes only. Please do not share it with anyone.</p>`,
+        });
+        return true;
+    }catch(error){
+        console.error("Error sending OTP: ",error);
+        return false;
+    }
+};
+export default sendOTP;
