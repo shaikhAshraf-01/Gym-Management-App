c4fd5ecd95f7c1a73128d0d041df3ae5995dba40
Ashraf Shaikh <ashrafshaikh5683@gmail.com>
2026-08-02 23:55:53 +0530

    all schema done

---
+++ backend/models/Inquiry.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+const inquirySchema=new mongoose.Schema({
+    gym:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Gym",
+        required:true,
+    },
+    name:{
+        type:String,
+        required:true,
+        trim:true,
+    },
+    mobile:{
+        type:String,
+        required:true,
+        trim:true,
+    },
+    willingToJoin:{
+        type:String,
+        trim:true,
+    },
+},
+{timestamps:true})
+const Inquiry=mongoose.model("Inquiry",inquirySchema);
+export default Inquiry;
