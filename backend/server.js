c7ca5b65483eca3a3cc5460772aa895caa8bfb5b
Ashraf Shaikh <ashrafshaikh5683@gmail.com>
2026-08-13 19:27:04 +0530

    server solved

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,16 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+import cors from "cors";
+import connectDB from "./config/db.js";
+
+import authRoutes from "./routes/authRoutes.js";
+import adminRoutes from "./routes/adminRoutes.js";
+import ownerRoutes from "./routes/ownerRoutes.js"
+connectDB();
+const app=express();
+
 const allowedOrigins = [
   process.env.CLIENT_ORIGIN, // Aapki website ka URL
   "http://localhost",        // Android local server
@@ -17,3 +30,20 @@ app.use(cors({
   },
   credentials: true
 }));
+
+
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/admin", adminRoutes);
+app.use("/api/owner",ownerRoutes);
+
+//for render to stay active
+app.get("/api/health", (req, res) => {
+  res.status(200).json({ status: "ok", time: new Date().toISOString() });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
